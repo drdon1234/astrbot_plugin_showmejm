b028b34a68319c7806547d0923f952a65265483e
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-08 21:15:46 +0800

    Update jm_options.py

--- utils/jm_options.py
+++ utils/jm_options.py
@@ -3,20 +3,20 @@ class JmOptions:
                  "auto_find_jm", "prevent_default", "option", "open_random_search", "group_whitelist", 'person_whitelist']
 
     def __init__(
-        self,
-        platform: str = 'napcat',
-        http_host: str = '192.168.5.2',
-        http_port: int = 13000,
-        token: str = '',
-        batch_size: int = 100,
-        pdf_max_pages: int = 200,
-        group_folder: str = '/',
-        auto_find_jm: bool = True,
-        prevent_default: bool = True,
-        option: str = str(Path(__file__).parent.parent / "config.yml"),
-        open_random_search: bool = True,
-        group_whitelist: list = None,
-        person_whitelist: list = None
+            self,
+            platform: str = 'napcat',
+            http_host: str = 'localhost',
+            http_port: int = 2333,
+            token: str = '',
+            batch_size: int = 100,
+            pdf_max_pages: int = 200,
+            group_folder: str = '/',
+            auto_find_jm: bool = True,
+            prevent_default: bool = True,
+            option: str = 'plugins/ShowMeJM/config.yml',
+            open_random_search: bool = True,
+            group_whitelist: list = None,
+            person_whitelist: list = None
     ):
         self.platform = platform
         self.http_host = http_host
@@ -33,4 +33,19 @@ class JmOptions:
         self.person_whitelist = person_whitelist
 
     @classmethod
-    def
+    def from_dict(cls, config: dict):
+        return cls(
+            platform=config.get('platform', 'napcat'),
+            http_host=config.get('http_host', 'localhost'),
+            http_port=config.get('http_port', 2333),
+            token=config.get('token', ''),
+            batch_size=config.get('batch_size', 100),
+            pdf_max_pages=config.get('pdf_max_pages', 200),
+            group_folder=config.get('group_folder', '/'),
+            auto_find_jm=config.get('auto_find_jm', True),
+            prevent_default=config.get('prevent_default', True),
+            option=config.get('option', 'plugins/ShowMeJM/config.yml'),
+            open_random_search=config.get('open_random_search', True),
+            group_whitelist=config.get('group_whitelist', []),
+            person_whitelist=config.get('person_whitelist', [])
+        )
