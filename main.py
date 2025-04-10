11734eeb6e2826305a14b63f4f37591622c4d2e2
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-11 07:54:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from astrbot.core.star.filter.event_message_type import EventMessageType
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
